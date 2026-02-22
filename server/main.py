ce60f298d63e981170ab43c42ed3289b3f684842
Lucas Christiansson <98053131+madebylucke@users.noreply.github.com>
2026-02-22 13:14:47 +0100

    feat: added scroll and qrcode generation

--- server/main.py
+++ server/main.py
@@ -1,7 +1,10 @@
 import os
+from socket import socket
 from flask import Flask, send_file
 from flask_socketio import SocketIO
 import pyautogui
+import socket
+import qrcode
 
 pyautogui.PAUSE = 0 
 
@@ -23,12 +26,29 @@ def handle_move(data):
     except pyautogui.FailSafeException:
         pass
 
+@socketio.on('scroll')
+def handle_scroll(data):
+    dy = float(data.get('dy', 0)) * 0.5
+    pyautogui.scroll(dy)
+
 @socketio.on('click')
 def handle_click(data):
     button_type = data.get('button', 'left') 
     pyautogui.click(button=button_type)
 
+
 if __name__ == '__main__':
     print("🚀 Magic Trackpad Server is running!")
     print("Ensure your phone is on the same Wi-Fi network.")
+
+    ip_addresses = socket.gethostbyname_ex(socket.gethostname())[2]
+    filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
+
+    img = qrcode.make(filtered_ips[0])
+    qr = qrcode.QRCode()
+
+    qr.add_data('http://' + filtered_ips[0] + ':3000')
+
+    qr.print_ascii()
+    
     socketio.run(app, host='0.0.0.0', port=3000)
